100e57c8f9a16ef5304c0a00691ee7bf03726576
Luisbert <Luisbert2506@gmail.com>
2019-08-03 17:59:45 -0400

    Added Proposal Component

---
+++ src/components/Proposal/Proposal.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Proposal.sass";
+
+const Proposal = () => (
+  <main>
+    <h1 />
+  </main>
+);
+
+export default Proposal;
--- src/containers/MainPage/MainPage.jsx
+++ src/containers/MainPage/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Header } from "../../components/Header";
+import { Proposal } from "../../components/Proposal";
 import "./MainPage.sass";
 
 const MainPage = () => (
   <Container>
     <Header />
+    <Proposal />
   </Container>
 );
 
