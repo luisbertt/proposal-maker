312f475db37c3f3846ab1f802ea0544c0bb8bd05
Luisbert <Luisbert2506@gmail.com>
2019-08-03 16:43:24 -0400

    Added buttons to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { Logo } from "../Logo";
 import LogoSrc from "../../assets/img/logo-horizontal.png";
+import { Button } from "react-bootstrap";
 import "./Header.sass";
 
 const Header = () => (
   <header>
     <Logo src={LogoSrc} height="60px" />
+    <div className="button-group">
+      <Button variant="dark">PREVIEW</Button>
+      <Button variant="primary">SAVE</Button>
+    </div>
   </header>
 );
 
