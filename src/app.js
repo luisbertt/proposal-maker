70695c2dee9348b9124a714242fec86a104b347c
Luisbert <Luisbert2506@gmail.com>
2019-08-03 15:23:21 -0400

    Added MainPage Container

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import { Container } from "react-bootstrap";
 import "./utils/main.sass";
+import { MainPage } from "./containers/MainPage";
 
 const App = () => (
   <Container>
-    <h1>Hello World!</h1>
+    <MainPage />
   </Container>
 );
 
---
+++ src/containers/MainPage/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "../../components/Header";
+
+const MainPage = () => (
+  <div>
+    <Header />
+  </div>
+);
+
+export default MainPage;
