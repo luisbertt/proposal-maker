e448e728ce986e6eae858f15a17361028c587f22
luisbertt <luisbert2506@gmail.com>
2019-08-29 15:45:10 -0400

    More style updates

--- src/components/InputGroup/InputGroup.jsx
+++ src/components/InputGroup/InputGroup.jsx
@@ -4,7 +4,7 @@ import "./InputGroup.sass";
 const InputGroup = ({ name, type }) => (
   <div className="form-group">
     <label>{name}</label>
-    <input className="form-control form-control-sm" type={type} />
+    <input className="form-control" type={type} />
   </div>
 );
 
