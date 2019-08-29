f1ed61d790917ba8edaf3cff551a4a27d5a99d64
luisbertt <luisbert2506@gmail.com>
2019-08-29 14:53:08 -0400

    Removed CompanyInfo Component from Proposal Component

--- src/components/Proposal/Proposal.jsx
+++ src/components/Proposal/Proposal.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import "./Proposal.sass";
-import { CompanyInfo } from "../CompanyInfo";
 import { ClientInfo } from "../ClientInfo";
 
 const Proposal = () => (
   <main>
-    <CompanyInfo />
     <ClientInfo />
   </main>
 );
