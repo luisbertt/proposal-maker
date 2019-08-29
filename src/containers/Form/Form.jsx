969237e48628d401b9e87ad3035c889ff4f6ad0d
luisbertt <luisbert2506@gmail.com>
2019-08-29 14:58:48 -0400

    Renamed Proposal Component to Form and moved it to containers directory

---
+++ src/containers/Form/Form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { ClientInfo } from "../../components/ClientInfo";
+import "./Form.sass";
+
+const Form = () => (
+  <main>
+    <ClientInfo />
+  </main>
+);
+
+export default Form;
--- src/containers/MainPage/MainPage.jsx
+++ src/containers/MainPage/MainPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { Header } from "../../components/Header";
-import { Proposal } from "../../components/Proposal";
+import { Form } from "../Form";
 import "./MainPage.sass";
 
 const MainPage = () => (
   <Container>
     <Header />
-    <Proposal />
+    <Form />
   </Container>
 );
 
