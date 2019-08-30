558d277f4f490ff6caea9d735bb5589d4b16f951
luisbertt <luisbert2506@gmail.com>
2019-08-29 23:06:58 -0400

    Added placeholder text for ScopeOfWork textarea

--- src/components/ScopeOfWork/ScopeOfWork.jsx
+++ src/components/ScopeOfWork/ScopeOfWork.jsx
@@ -4,7 +4,12 @@ import "./ScopeOfWork.sass";
 const ScopeOfWork = () => (
   <div className="scope-of-work">
     <h3 className="text-center">Scope Of Work</h3>
-    <textarea className="form-control" id="scopeOfWork" rows="3"></textarea>
+    <textarea
+      className="form-control"
+      id="scopeOfWork"
+      rows="3"
+      placeholder="Define scope of work"
+    ></textarea>
   </div>
 );
 
