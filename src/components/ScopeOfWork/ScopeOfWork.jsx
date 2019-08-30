9656b8321589d9fc2f43ad01e37c0bb015360f4b
luisbertt <luisbert2506@gmail.com>
2019-08-29 21:14:14 -0400

    Created ScopeOfWork Component

---
+++ src/components/ScopeOfWork/ScopeOfWork.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./ScopeOfWork.sass";
+
+const ScopeOfWork = () => (
+  <div className="scope-of-work">
+    <h4>Scope Of Work</h4>
+  </div>
+);
+
+export default ScopeOfWork;
---
+++ src/components/ScopeOfWork/index.js
@@ -0,0 +1,1 @@
+export { default as ScopeOfWork } from "./ScopeOfWork";
