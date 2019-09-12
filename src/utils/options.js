157ffaeb5902f0e80de622ed4efaadd002bdadba
luisbertt <luisbert2506@gmail.com>
2019-09-12 15:25:08 -0400

    Addded options util file

---
+++ src/utils/options.js
@@ -0,0 +1,39 @@
+export const exteriorOptions = [
+  "Body",
+  "Trim",
+  "Sofit",
+  "Fascia",
+  "Corbels",
+  "Gutters",
+  "Lanai",
+  "Lanai Ceiling",
+  "Front Door",
+  "Exterior Door",
+  "Garage Door",
+  "Garage Floor"
+];
+
+export const interiorOptions = [
+  "Walls",
+  "Ceilings",
+  "Bathroom",
+  "Bathroom 2",
+  "Bathroom 3",
+  "Bathroom 4",
+  "Master Bathroom",
+  "Bedroom",
+  "Bedroom 2",
+  "Bedroom 3",
+  "Bedroom 4",
+  "Tray Ceiling",
+  "Wall Details",
+  "Ceiling Details",
+  "Trim/Baseboard",
+  "Trim/Door",
+  "Trim",
+  "Casing",
+  "Door",
+  "Cabinets",
+  "Cabinet Door",
+  "Cabinet Box"
+];
