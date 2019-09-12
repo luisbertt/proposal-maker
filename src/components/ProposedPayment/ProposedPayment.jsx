d5df7df1dab090aedd4ab3332dad888287d9cbd4
luisbertt <luisbert2506@gmail.com>
2019-09-12 16:13:53 -0400

    Added state to ScopeOfWork & ProposedPayment Components

--- src/components/ProposedPayment/ProposedPayment.jsx
+++ src/components/ProposedPayment/ProposedPayment.jsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProposedPayment.sass";
 
-const ProposedPayment = () => (
-  <div className="proposed-payment mt-4">
-    <h3 className="text-center">Proposed Payment</h3>
-    <p className="text-center">
-      <b>OWNER</b> agress to pay <b>CONTRACTOR</b> a proposed total cash price
-      of <b>$0.00</b>. <b>OWNER</b> represents that this agreement is a cash
-      transaction where no financing is contemplated and <b>CONTRACTOR</b> acts
-      in reliance on said representation.
-    </p>
-    <p className="text-center">The payment schedule is as follows:</p>
-    <input
-      className="form-control text-center"
-      name="proposedPayment"
-      type="text"
-    />
-  </div>
-);
+const ProposedPayment = () => {
+  const [payment, setPayment] = useState("");
+
+  const handleChange = e => {
+    const newPayment = e.target.value;
+    setPayment(newPayment);
+  };
+
+  return (
+    <div className="proposed-payment mt-4">
+      <h3 className="text-center">Proposed Payment</h3>
+      <p className="text-center">
+        <b>OWNER</b> agress to pay <b>CONTRACTOR</b> a proposed total cash price
+        of <b>$0.00</b>. <b>OWNER</b> represents that this agreement is a cash
+        transaction where no financing is contemplated and <b>CONTRACTOR</b>{" "}
+        acts in reliance on said representation.
+      </p>
+      <p className="text-center">The payment schedule is as follows:</p>
+      <input
+        className="form-control text-center"
+        name="proposedPayment"
+        type="text"
+        onChange={handleChange}
+      />
+    </div>
+  );
+};
 
 export default ProposedPayment;
--- src/components/ScopeOfWork/ScopeOfWork.jsx
+++ src/components/ScopeOfWork/ScopeOfWork.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ScopeOfWork.sass";
 
-const ScopeOfWork = () => (
-  <div className="scope-of-work mt-4">
-    <h3 className="text-center">Scope Of Work</h3>
-    <textarea
-      className="form-control"
-      id="scopeOfWork"
-      rows="3"
-      placeholder="Define scope of work"
-    ></textarea>
-  </div>
-);
+const ScopeOfWork = () => {
+  const [scope, setScope] = useState("");
+
+  const handleChange = e => {
+    const newScope = e.target.value;
+    setScope(newScope);
+  };
+
+  return (
+    <div className="scope-of-work mt-4">
+      <h3 className="text-center">Scope Of Work</h3>
+      <textarea
+        className="form-control"
+        id="scopeOfWork"
+        rows="3"
+        placeholder="Define scope of work"
+        onChange={handleChange}
+      ></textarea>
+    </div>
+  );
+};
 
 export default ScopeOfWork;
